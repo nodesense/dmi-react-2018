2d250199a90bed6761c50584c2a29146816e332d
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-06-15 16:29:32 +0530

    updated cart

---
+++ src/app/cart/components/Cart.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default class Cart extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            items: [
+                {id: 1, name: "Product 1", price: 100, qty: 1}
+            ]
+        }
+    }
+
+    //Es.NEXT 
+    addItem = () => {
+        let id = Math.ceil(Math.random() * 100000)
+
+        let item = {
+            id, //id: id (es6 syle)
+            name: `Product ${id}`,
+            price: 1 + Math.ceil(Math.random() * 100),
+            qty: 1
+        }
+    }
+  
+    removeItem = (id) => {
+        
+    }
+
+    updateItem = (id, qty) => {
+        
+    }
+
+    empty = () => {
+        
+    }
+
+    dummyRefresh = () => {
+        this.setState({
+            flag: true
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.addItem}>
+                    Add Item
+                </button>
+                <button onClick={this.empty}>
+                    Empty
+                </button>
+                <button onClick={this.dummyRefresh}>
+                    Refresh
+                </button>
+            </div>
+        )
+    }
+}
