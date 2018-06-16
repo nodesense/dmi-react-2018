e92c4b843c18fb2adfdcc3a079950b9da9ba411f
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-06-16 13:06:16 +0530

    default and propTypes

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import PropTypes from "prop-types";
+
 // just calls method to get v.dom
 export default function Footer(props) {
     console.log("Footer render")
@@ -14,4 +16,16 @@ export default function Footer(props) {
             <p>Copyrights@ {props.title}, {props.year} </p>
         </div>
     )
+}
+
+//Keywords
+
+Footer.propTypes = {
+    title: PropTypes.string,
+    year: PropTypes.number.isRequired // mandatory
+}
+
+//keyword
+Footer.defaultProps = {
+    title: 'React App'
 }
